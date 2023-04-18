4b4ddc82b19ba3e44eebc1a148769fecf2a0383a
NeeNawAmbulance <51728263+NeeNawAmbulance@users.noreply.github.com>
2023-04-18 15:23:44 +0800

    made clock workable, laid down function foundations

--- clock.js
+++ clock.js
@@ -4,22 +4,38 @@
 // coordinate of clock is centre of clock
 
 class Clock {
-    constructor(x, y) {
-      this.radius = 15;
-      this.position = createVector(x, y);
-      this.angularVelocity = 0;
-      this.angularAcceleration = 0;
-    }
-
-    display() {
-        stroke(255);
-        strokeWeight(2);
-        fill(255);
-        ellipse(this.position.x, this.position.y, this.radius * 2);
-    }
-
-    setRotSpeed(speed) {
-        this.angularVelocity = speed;
-    }
+  constructor(x, y) {
+    this.radius = 15;
+    this.position = createVector(x, y);
+    this.angularVelocity1 = 0; // in degrees per frame
+    this.angularVelocity2 = 0;
+    this.angle1 = 0;
+    this.angle2 = 0;
   }
+
+  display() {
+    push();
+    translate(this.position.x, this.position.y);
+    stroke(255);
+    strokeWeight(2);
+    fill(255);
+    ellipse(0, 0, this.radius * 2);
+    stroke(0);
+    line(0, 0, this.radius * sin(this.angle1), this.radius * cos(this.angle1));
+    line(0, 0, this.radius * sin(this.angle2), this.radius * cos(this.angle2));
+    pop();
+  }
+
+  setRotSpeed(v1, v2) {
+    this.angularVelocity1 = v1;
+    this.angularVelocity2 = v2;
+  }
+
+  update() {
+    this.angle1 += this.angularVelocity1;
+    this.angle2 += this.angularVelocity2
+    line(0, 0, this.radius * sin(this.angle1), this.radius * cos(this.angle1));
+    line(0, 0, this.radius * sin(this.angle2), this.radius * cos(this.angle2));
+  }
+}
   
--- sketch.js
+++ sketch.js
@@ -1,27 +1,39 @@
 // 14 * 36 grid of clocks
 
 let clockArr = [];
+let noiseMult = 0.02
 
 function setup() {
   createCanvas(1285, 515); // each clock is 30*30, gap of 5, ends are 20
-}
+  angleMode(DEGREES);
 
-function draw() {
-  // setup and display clock array
-  background(243, 243, 243);
+  // init 2D array
   for (let i = 0; i < 36; i++) {
     clockArr[i] = [];
     for (let j = 0; j < 14; j++) {
       clockArr[i][j] = new Clock(20 + 15 + 30 * i + 5 * (i - 1), 20 + 15 + 30 * j + 5 * (j - 1));
-      clockArr[i][j].display();
     }
   }
+}
+
+function draw() {
+  background(243, 243, 243);
 
   // update clock array
   for (let i = 0; i < 36; i++) {
     for (let j = 0; j < 14; j++) {
       currClock = clockArr[i][j];
-      currClock.setRotSpeed(noise(currClock.position.x * 0.2, currClock.position.y * 0.2));
+      currClock.display();
+      currClock.setRotSpeed(noise(i * noiseMult, j * noiseMult), -noise(i * noiseMult, j * noiseMult));
+      currClock.update();
     }
   }
+}
+
+function normalPerlin() {
+
+}
+
+function mirroredPerlin() {
+  
 }
